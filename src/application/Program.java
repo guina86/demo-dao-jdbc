2ba8a0f82c135749b28e82156f17f57ea88392e0
Leandro Guina <guina86@gmail.com>
2019-03-26 19:43:54 -0300

    Adicionado SellerDaoJDBC e DaoFactory classes

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.time.LocalDate;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,7 +13,12 @@ public class Program {
 		
 		Department dep = new Department(1, "Books");
 		Seller seller = new Seller(21, "bob", "bob@gmail.com", LocalDate.of(1985,05,12), 3000.0, dep );
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
+		
+		
 
 	}
 
