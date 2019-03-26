fe2904f29d5406c15f31b911a3e62ba215493c27
Leandro Guina <guina86@gmail.com>
2019-03-26 20:59:22 -0300

    Adicionado implementacao do findById em SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,16 @@
 package application;
 
-import java.time.LocalDate;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dep = new Department(1, "Books");
-		Seller seller = new Seller(21, "bob", "bob@gmail.com", LocalDate.of(1985,05,12), 3000.0, dep );
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 		
