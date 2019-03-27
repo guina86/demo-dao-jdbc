4a70f2c713dfe16003d82894c3f5dd9167ba52bf
Leandro Guina <guina86@gmail.com>
2019-03-26 23:25:35 -0300

    Adicionado implementacao do insert em SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -10,29 +11,33 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("=== TEST 1: seller findById =====");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
+
 		System.out.println();
-		
+
 		System.out.println("=== TEST 2: seller findByDepartment =====");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		list.forEach(System.out::println);
 
 		System.out.println();
-		
+
 		System.out.println("=== TEST 3: seller findAll =====");
 		list = sellerDao.findAll();
 		list.forEach(System.out::println);
-		
-		
-		
 
+		System.out.println();
+
+		System.out.println("=== TEST 3: seller insert =====");
+		Seller newSeller = new Seller(null, "Greg Green", "greg@gmail.com", LocalDate.of(1990, 10, 10), 4000.0, department );
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+		
 	}
 
 }
