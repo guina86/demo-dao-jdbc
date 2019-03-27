c54617e540d1911d51d05c6ecd948347fe02883c
Leandro Guina <guina86@gmail.com>
2019-03-27 14:16:14 -0300

    Adicionado DepartmentDaoJDBC e implmentacao do findById

---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,63 @@
+package model.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import db.DbException;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+	
+	private Connection conn;
+	
+	public DepartmentDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+
+	@Override
+	public void insert(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		
+		try(PreparedStatement st = conn.prepareStatement("SELECT department.* FROM department WHERE id = ?")) {
+			st.setInt(1, id);
+			ResultSet rs = st.executeQuery();
+			if(rs.next()) {
+				return new Department(id, rs.getString(2));
+			}
+			return null;
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+
+	}
+
+	@Override
+	public List<Department> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
